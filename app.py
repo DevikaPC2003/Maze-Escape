8806646d863530aa08c7a81a7703af055e9b79ee
Devika chettayil <152673304+DevikaPC2003@users.noreply.github.com>
2025-08-09 06:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def game():
     return render_template('index.html')  # Maze Game
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
